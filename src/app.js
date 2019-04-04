3feda982245f411c80cda3f9bc9f2583046b5145
Štěpán Śevčík <Thoronir42@Gmail.com>
2019-04-04 09:43:25 +0200

    FIxed static files serving usage bla bla bla Kuba.

--- src/app.js
+++ src/app.js
@@ -26,15 +26,17 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
+    src: path.join(__dirname, '..', 'sass'),
+    dest: path.join(__dirname, '..', 'public/stylesheets'),
+    prefix: '/stylesheets',
+    indentedSyntax: false, // true = .sass and false = .scss
+    sourceMap: true,
 }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Routing
 const ModularRouter = require('./routes/ModularRouter');
@@ -51,19 +53,19 @@ app.use('/users', usersRouter);
 app.use('/', router.compileRouter());
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
